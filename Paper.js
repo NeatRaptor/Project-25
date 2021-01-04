66d5cbe5677832eb0c7d387ac90887468e549002
Roshan K <74712665+NeatRaptor@users.noreply.github.com>
2021-01-04 16:06:35 +0530

    Update Paper.js

--- Paper.js
+++ Paper.js
@@ -10,7 +10,7 @@ class Paper {
         this.y=y;
         this.r=r;
         this.body=Bodies.circle(this.x,this.y,this.r/2,options)
-        this.image = loadImage("sprite/paper.png")
+        this.image = loadImage("paper.png")
         World.add(world,this.body,this.image)
     }
     display() {
